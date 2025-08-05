6827aa9267adce61cb30a1d0c04052098be5c1fd
Sam Simpson <sam@simpson.id>
2025-08-05 20:52:30 +0100

    Add pocket-id and firefly config for caddy

--- apps/autorestic/autorestic.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-  echo "Start backup"
-  /usr/bin/autorestic backup -v -a -c /autorestic.yaml
-  echo "Sleeping for ${BACKUP_INTERVAL_SECONDS}s"
-  sleep ${BACKUP_INTERVAL_SECONDS}
-done
